431d1bd7821bcf1fd7ad67bd4439222392876452
TJ Taylor <Thomas_Taylor2@cable.comcast.com>
2013-02-24 21:45:34 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InvestmentGame::Application.config.secret_token = 'c4319566f741c1cdc0f9e3b4443186ad4ea7ce7df4a5c93f253e5ddb3ec0712fc54e2a7955b15370e50a11532f5802dee2baeecb60de08997512c1463d7caaf1'
